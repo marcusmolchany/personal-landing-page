05a4b28db968e55dccfaa279df38ab590d920a6f
marcusmolchany <marcusmolchany@gmail.com>
2014-10-09 22:00:44 -0400

    added animations and placeholder images for showcase section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 var main = function() {
   animateSocialIcons();
+
+  $(window).scroll(function() {
+    var topOfWindow = $(window).scrollTop();
+
+    var $showcaseSection = $(".image-placeholder");
+    if (topOfWindow >= 750 && !$showcaseSection.hasClass("animate")) {
+      $showcaseSection.addClass("animate");
+    } else if (topOfWindow < 750) {
+      $showcaseSection.removeClass("animate");
+    }
+  });
 }
 
 var animateSocialIcons = function() {
