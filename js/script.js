46092e32a3983ad5c6f30de95ed7b59c4108edda
marcusmolchany <marcusmolchany@gmail.com>
2014-10-10 00:06:47 -0400

    added parallax scrolling to interests section

--- js/script.js
+++ js/script.js
@@ -3,12 +3,21 @@ var main = function() {
 
   $(window).scroll(function() {
     var topOfWindow = $(window).scrollTop();
+    console.log(topOfWindow);
 
     var $showcaseSection = $(".image-placeholder");
     if (topOfWindow >= 750 && !$showcaseSection.hasClass("animate")) {
       $showcaseSection.addClass("animate");
-    } else if (topOfWindow < 750) {
+    } else if (topOfWindow < 400) {
       $showcaseSection.removeClass("animate");
+    }
+
+    var $interestsSectionParagraph = $("#interests-section p");
+    if (topOfWindow >= 2000) {
+
+    } else if (topOfWindow >= 1300) {
+      var topOffset = (topOfWindow - 1100) / 4;
+      $interestsSectionParagraph.css({'top': topOffset});
     }
   });
 }
