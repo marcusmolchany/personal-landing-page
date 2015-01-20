c2f92fdf5aa11a263af8ab6c89a0ebfcd1d4f3ae
marcusmolchany <marcusmolchany@gmail.com>
2015-01-20 00:15:18 -0500

    Fixed <video> for Firefox. Changes for page load optimization

--- js/app.js
+++ js/app.js
@@ -20,6 +20,15 @@
         $interestsSectionParagraph.css({'top': topOffset});
       }
     });
+
+    var width = $(window).width();
+    if (width > 800) {
+      var $video = $('<video autoplay  poster="media/background-img.jpg" id="bgvid" loop>
+                        <source src="media/background-video.webm" type="video/webm">
+                        <source src="media/background-video.mp4" type="video/mp4">
+                      </video>');
+      $("#greeting-section").prepend($video);
+    }
   }
 
   var animateSocialIcons = function() {
